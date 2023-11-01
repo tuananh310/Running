1f7ffd9f0521983495f1b0718f81f23dc2d160cb
ta <tuananh31020@gmail.com>
2023-11-01 21:10:14 +0700

    v

--- Assets/Scripts/ThirdPersonMovement.cs
+++ Assets/Scripts/ThirdPersonMovement.cs
@@ -20,8 +20,10 @@ public class ThirdPersonMovement : MonoBehaviour
 
         if (direction.magnitude >= 0.1f) // nếu độ lớn của hướng >= 0.1
         {
-            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y; // xác định góc mục tiêu muốn nhân vật xoay
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg; // xác định góc mục tiêu muốn nhân vật xoay
+            // float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y; // xác định góc mục tiêu muốn nhân vật xoay
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            // transform.rotation = Quaternion.Euler(0f, targetAngle, 0f); // xoay nhân vật
             transform.rotation = Quaternion.Euler(0f, angle, 0f); // xoay nhân vật
 
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
