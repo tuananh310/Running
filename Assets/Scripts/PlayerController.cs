8c0ad7387c0954a6a27c18974a88729487537b21
ta <tuananh31020@gmail.com>
2023-11-07 11:11:13 +0700

    ta

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,24 +1,43 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Rendering.LookDev;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(CharacterController))]
 public class PlayerController : MonoBehaviour
 {
+    #region Variables: Movement
     private Vector2 _input;
     [SerializeField] private Transform cam; // main camera
     private CharacterController _characterController;
     private Vector3 _direction;
+    [SerializeField] private float speed;
 
+    [SerializeField] private Movement movement;
+
+    #endregion
+
+    #region Variables: Rotation
     [SerializeField] private float smoothTime = 0.05f;
     private float _currentVelocity;
 
-    [SerializeField] private float speed;
+    #endregion
 
+    #region Variables: Gravity
     private float _gravity = -9.81f;
     [SerializeField] private float gravityMultiplier = 3.0f;
     private float _velocity;
+
+    #endregion
+
+    #region Variables: Jump
+    [SerializeField] private float jumpPower;
+    private int _numberOfJumps;
+    [SerializeField] private int maxNumberOfJumps = 2;
+
+    #endregion
 
     private void Awake()
     {
@@ -34,7 +53,7 @@ public class PlayerController : MonoBehaviour
 
     private void ApplyGravity()
     {
-        if (_characterController.isGrounded && _velocity < 0f)
+        if (IsGrounded() && _velocity < 0f)
         {
             _velocity = -1.0f;
         }
@@ -57,7 +76,10 @@ public class PlayerController : MonoBehaviour
 
     private void ApplyMovement()
     {
-        _characterController.Move(_direction * speed * Time.deltaTime); // Di chuyển nhân vật
+        var targetSpeed = movement.isSprinting ? movement.speed * movement.multiplier : movement.speed;
+        movement.currentSpeed = Mathf.MoveTowards(movement.currentSpeed, targetSpeed, movement.acceleration * Time.deltaTime);
+
+        _characterController.Move(_direction * movement.currentSpeed * Time.deltaTime); // Di chuyển nhân vật
     }
 
     public void Move(InputAction.CallbackContext context)
@@ -65,4 +87,41 @@ public class PlayerController : MonoBehaviour
         _input = context.ReadValue<Vector2>();
         _direction = new Vector3(_input.x, 0f, _input.y);
     }
+
+    public void Jump(InputAction.CallbackContext context)
+    {
+        if (!context.started) return;
+        if (!IsGrounded() && _numberOfJumps >= maxNumberOfJumps) return;
+        if (_numberOfJumps == 0) StartCoroutine(WaitForLanding());
+
+        _numberOfJumps++;
+        _velocity = jumpPower;
+    }
+
+    public void Sprint(InputAction.CallbackContext context)
+    {
+        movement.isSprinting = context.started || context.performed;
+    }
+
+    private IEnumerator WaitForLanding()
+    {
+        yield return new WaitUntil(() => !IsGrounded());
+        yield return new WaitUntil(IsGrounded);
+
+        _numberOfJumps = 0;
+    }
+
+    private bool IsGrounded() => _characterController.isGrounded;
 }
+
+[Serializable]
+public struct Movement
+{
+    public float speed;
+    public float multiplier;
+    public float acceleration; // gia tốc
+    [HideInInspector] public bool isSprinting;
+    [HideInInspector] public float currentSpeed;
+}
+
+
